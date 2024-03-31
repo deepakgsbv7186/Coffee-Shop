d7c4df3aac6b5fcfe9806f98b3d3558f64991443
deepak.verma <deekgsbv@gmail.com>
2024-03-31 14:37:14 +0530

    Custom Icon setup with selection.json file
    Note: Only install react-native-vector-icons no setup required
    
    Working on Android device

--- src/navigators/BottomTab.jsx
+++ src/navigators/BottomTab.jsx
@@ -1,9 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import CustomIcon from '../utils/CustomIcon';
+import {COLORS} from '../theme';
 
 export default function BottomTab() {
   return (
-    <View>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <CustomIcon name="home" size={25} color={COLORS.primaryOrangeHex} />
       <Text>BottomTab</Text>
     </View>
   );
