26494c116b550bb80671088311b5b5d154927482
deepak.verma <deekgsbv@gmail.com>
2024-03-31 16:12:13 +0530

    app name change to "Coffee Shop", add app icon and setup splash screen shows for two seconds

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import {StatusBar} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import RootNavigation from './navigators/RootNavigation';
 import {COLORS} from './theme';
 
 export default function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+  }, []);
   return (
     <>
       <StatusBar
