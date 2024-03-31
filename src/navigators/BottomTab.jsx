21682ae316d0e02de72a110b8fc342e051fd3fd7
deepak.verma <deekgsbv@gmail.com>
2024-03-31 15:00:29 +0530

    add blur bottom tab with icons, auto keyboard hide when user type

--- src/navigators/BottomTab.jsx
+++ src/navigators/BottomTab.jsx
@@ -1,15 +1,104 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar} from 'react-native';
 import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {BlurView} from '@react-native-community/blur';
 import CustomIcon from '../utils/CustomIcon';
 import {COLORS} from '../theme';
+import Home from '../screens/Home';
+import Favorites from '../screens/Favorites';
+import Cart from '../screens/Cart';
+import OrderHistory from '../screens/OrderHistory';
 
+const Bottom = createBottomTabNavigator();
 export default function BottomTab() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <CustomIcon name="home" size={25} color={COLORS.primaryOrangeHex} />
-      <Text>BottomTab</Text>
-    </View>
+    <Bottom.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: 'absolute',
+          elevation: 0,
+          borderTopColor: 'transparent',
+          borderTopWidth: 0,
+          backgroundColor: COLORS.primaryBlackRGBA,
+          height: StatusBar.currentHeight * 2 + 10,
+        },
+        tabBarBackground: () => (
+          <BlurView
+            overlayColor=""
+            blurAmount={15}
+            style={{
+              position: 'absolute',
+              top: 0,
+              right: 0,
+              left: 0,
+              bottom: 0,
+            }}
+          />
+        ),
+      }}>
+      <Bottom.Screen
+        component={Home}
+        name="Home"
+        options={{
+          tabBarIcon: ({focused}) => (
+            <CustomIcon
+              name="home"
+              size={24}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}
+      />
+      <Bottom.Screen
+        component={Favorites}
+        name="Favorites"
+        options={{
+          tabBarIcon: ({focused}) => (
+            <CustomIcon
+              name="like"
+              size={24}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}
+      />
+      <Bottom.Screen
+        component={Cart}
+        name="Cart"
+        options={{
+          tabBarIcon: ({focused}) => (
+            <CustomIcon
+              name="cart"
+              size={24}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}
+      />
+      <Bottom.Screen
+        component={OrderHistory}
+        name="OrderHistory"
+        options={{
+          tabBarIcon: ({focused}) => (
+            <CustomIcon
+              name="bell"
+              size={24}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}
+      />
+    </Bottom.Navigator>
   );
 }
-
-const styles = StyleSheet.create({});
